974af40e8c2de60258fc059f6d906787b40e8f20
Sophie Jones <sophiejones@rockborne.com>
2023-02-03 15:32:34 +0000

    Make clear nav button is a file

--- Home.py
+++ Home.py
@@ -7,7 +7,7 @@ from streamlit_autorefresh import st_autorefresh
 import datetime 
 from datetime import date
 from dateutil.relativedelta import relativedelta
-from .util.navbutton import *
+from streamlit_oura.util.navbutton import *
 
 # update every 5 mins
 st_autorefresh(interval=1 * 60 * 1000, key="dataframerefresh")
